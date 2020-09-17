1c694100409c6ed63575d157ba865204956bbd86
Melissa Astbury <melissajastbury@gmail.com>
2020-09-17 11:14:51 +0100

    learnt that the order of passing the properties of the object matter as the properties may get mixed up and conflict

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,12 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
-  console.log(`
-${id} ${title} ${completed}`);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it completed? ${completed}`);
+};
