c04e9ce8df85ba20493219ef2ed4b2ff8cdf96ae
Melissa Astbury <melissajastbury@gmail.com>
2020-09-25 09:26:47 +0100

    added and understood how functions work inside an interface

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -6,18 +6,22 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  sold: Date;
+  summary(): string;
 }
 
 const oldFord = {
   name: "focus",
   year: 1998,
   broken: true,
+  sold: new Date(),
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldFord);
